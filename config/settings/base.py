1443942cca5e544a5273e110edda26e598e70538
djsmedes <djsmedes@gmail.com>
2018-07-08 15:47:11 -0500

    first attempt at setting up templates

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,7 +68,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,7 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
---
+++ config/settings/prod.py
@@ -0,0 +1,6 @@
+from .base import *
+
+# automatically redirect to HTTPS, and other SSL stuff
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
