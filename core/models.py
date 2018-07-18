849eb65c3cfffbed97276c4d270dd67a68b7934d
djsmedes <djsmedes@gmail.com>
2018-07-18 08:55:22 -0500

    creating basic location mixins

--- core/models.py
+++ core/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+
+from .constants import State
+
+
+class AddressMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    full_address_raw = models.CharField(max_length=511, null=True, blank=True)
+    street_address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=127, null=True, blank=True)
+    state = models.CharField(max_length=2, choices=State.MODEL_FIELD_CHOICES, null=True, blank=True)
+    zip_code = models.IntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        address_array = []
+        if self.street_address:
+            address_array.append(self.street_address)
+        if self.city:
+            address_array.append(self.city)
+        if self.state:
+            address_array.append(self.state)
+        if self.zip_code:
+            address_array.append(self.zip_code)
+        if address_array:
+            self.full_address_raw = " ".join(address_array)
+        super().save(*args, **kwargs)
+
+
+class LatLonMixin(models.Model):
+    """Not tested"""
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+
