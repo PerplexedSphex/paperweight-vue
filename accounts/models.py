431c11467cc68276bf0e608fb0506be50dc520c5
djsmedes <djsmedes@gmail.com>
2018-07-18 08:55:00 -0500

    creating AccountHolder model

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,16 @@ from authtools.models import AbstractNamedUser
 
 class EncampUser(AbstractNamedUser):
     """Creating this now so it's easier to customize later without borking the db"""
-    pass
+
+    def __str__(self):
+        return self.name
+
+
+class AccountHolder(models.Model):
+    """Basic tenant entity for multitenanting.
+    All records except for users should have a foreign key to this model."""
+    name = models.CharField(max_length=255)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
